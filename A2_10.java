93f63c7c66ebea7ee231d2f4c470fc9484be15a5
Siddhant <siddhantc08@gmail.com>
2024-12-18 19:12:03 +0530

    Reverting "adding the gpt code and re-modifying the variable"
    This reverts commit 7b79372736494321cf508bbe93fdf51dd714ef50.

--- A2_10.java
+++ A2_10.java
@@ -1,9 +1,6 @@
 public class A2_10 {
     public static void main(String[] args) {
-    int a=824;
-    int temp=0;
-    int var;
-    int b=1;
+    int a=824,temp=0,var,b=1;
     var=a%10;
     int c=a/10;
     int copy=c;
@@ -20,3 +17,22 @@ public class A2_10 {
         System.out.println(a);
     }
 }
+/**
+ * By GPT
+ * public class A2_10 {
+ *     public static void main(String[] args) {
+ *         int a = 824;
+ *         int numDigits = (int) Math.log10(a) + 1;  // Calculate the number of digits
+ *         int lastDigit = a % 10;  // Extract the last digit
+ *         int remainingDigits = a / 10;  // Remove the last digit
+ *
+ *         // Calculate the rotated number
+ *         int rotatedNumber = lastDigit * (int) Math.pow(10, numDigits - 1) + remainingDigits;
+ *
+ *         // Print the rotated number
+ *         System.out.println(rotatedNumber);  // Should print 482
+ *     }
+ * }
+ *
+ *
+ * */
